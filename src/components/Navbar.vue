d269336647c40f1673269e65f56e85eae0057fbf
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-09-09 11:32:29 +0530

    Created Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template
+  ><nav class="navbar navbar-expand navbar-light bg-light">
+    <ul class="nav navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"
+          >Home <span class="sr-only">(current)</span></a
+        >
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Workshops List</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Feedback</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style scoped></style>
