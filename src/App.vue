9d3bfd9f52d8bdb35d68d52e25b126fbf2e438c1
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-09-09 11:33:17 +0530

    import Navbar.vue and display navbar on homescreen

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <div id="app" class="container">
-    Hello Vue
+  <div id="app">
+    <app-navbar></app-navbar>
+    <div class="container">
+      Hello Vue
+    </div>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue';
+
 export default {
   name: 'App',
+  components: {
+    AppNavbar: Navbar,
+  },
 };
 </script>
 
-<style>
-#app {
-  border: 1px solid black;
-}
-</style>
+<style></style>
