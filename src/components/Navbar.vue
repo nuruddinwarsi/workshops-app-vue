316b33c2f83163c7ef50d46ea2d5501b8a8ee214
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-09-14 17:45:41 +0530

    Added exact to switch nav link highlighting

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,11 +2,15 @@
   ><nav class="navbar navbar-expand navbar-light bg-light">
     <div class="container px-2">
       <ul class="nav navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="/">Home</a>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/" active-class="active" exact
+            >Home</router-link
+          >
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/workshops">Workshops List</a>
+          <router-link class="nav-link" to="/workshops" active-class="active"
+            >Workshops List</router-link
+          >
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Feedback</a>
