aadd765e12ab282886791a2767c9f77056f211ac
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-09-14 18:53:30 +0530

    Used axios to get workshop data and added loading icon

--- src/components/WorkshopsList.vue
+++ src/components/WorkshopsList.vue
@@ -1,12 +1,48 @@
 <template>
   <div>
-    This is workshops list
+    <h1>List of workshops</h1>
+    <hr />
+    <div class="d-flex justify-content-center" v-if="status === 'LOADING'">
+      <div class="spinner-border" role="status">
+        <!-- <span class="sr-only">Loading...</span> -->
+      </div>
+    </div>
+    <div v-else-if="status === 'LOADED'">
+      <ul>
+        <li v-for="workshop in workshops" :key="workshop.id">
+          {{ workshop.name }}
+        </li>
+      </ul>
+    </div>
+    <div v-else-if="status === 'ERROR'" class="alert alert-danger" role="alert">
+      {{ error.message }}
+    </div>
   </div>
 </template>
 
 <script>
+import { getWorkshops } from '@/services/workshops.js';
+
 export default {
   name: 'WorkshopsList',
+  data() {
+    return {
+      workshops: [],
+      status: 'LOADING',
+      error: null,
+    };
+  },
+  created() {
+    getWorkshops()
+      .then((data) => {
+        this.workshops = data;
+        this.status = 'LOADED';
+      })
+      .catch((error) => {
+        this.status = 'ERROR';
+        this.error = error;
+      });
+  },
 };
 </script>
 
---
+++ src/services/workshops.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+function getWorkshops() {
+  return axios
+    .get(`https://workshops-server.herokuapp.com/workshops/`)
+    .then((response) => response.data);
+}
+
+export { getWorkshops };
