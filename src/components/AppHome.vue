bf387dd2ebdb5546aebad37d19c6fccc8b6bf0f5
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-09-14 17:55:22 +0530

    Added Homepage content

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    This is Homepage
+  <div class="container">
+    <div class="jumbotron">
+      <h1>Welcome to Workshops App</h1>
+      <hr />
+      <p class="lead">
+        Welcome to the workshops app. You can find the details of the technical
+        workshops happening nearby
+        <router-link to="/workshops">here</router-link>
+      </p>
+    </div>
   </div>
 </template>
 
