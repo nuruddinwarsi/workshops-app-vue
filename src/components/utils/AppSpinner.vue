119295ffe1397e88173360a891130f0a7d455635
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-09-15 09:40:34 +0530

    Added theme mixin for AppSpinner

--- src/components/utils/AppSpinner.vue
+++ src/components/utils/AppSpinner.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="d-flex justify-content-center">
-    <div class="spinner-border" :class="`spinnerSizeClass`" role="status">
+    <div
+      class="spinner-border"
+      :class="`[spinnerSizeClass,spinnerThemeClass]`"
+      role="status"
+    >
       <span class="sr-only">Loading...</span>
     </div>
   </div>
 </template>
 
 <script>
+import ThemeMixin from '@/mixins/theme';
+
 export default {
+  name: 'AppSpinnner',
+  mixins: [ThemeMixin],
   props: {
     size: {
       type: String,
@@ -20,6 +28,18 @@ export default {
         'spinner-border-large': this.size === 'large',
         'spinner-border-medium': this.size === 'medium',
         'spinner-border-small': this.size === 'small',
+      };
+    },
+    spinnerThemeClass() {
+      return {
+        'text-primary': this.theme === 'primary',
+        'text-secondary': this.theme === 'secondary',
+        'text-success': this.theme === 'success',
+        'text-info': this.theme === 'info',
+        'text-warning': this.theme === 'warning',
+        'text-danger': this.theme === 'danger',
+        'text-dark': this.theme === 'dark',
+        'text-light': this.theme === 'light',
       };
     },
   },
